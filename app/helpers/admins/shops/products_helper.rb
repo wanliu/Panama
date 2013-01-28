124e83683c8268d1b540d68e6e844f64380a826a
kaka <huxinghai1988@gmail.com>
2013-01-28 10:34:20 +0800

    fixed product helper

--- app/helpers/admins/shops/products_helper.rb
+++ app/helpers/admins/shops/products_helper.rb
@@ -1,10 +1,10 @@
 module Admins::Shops::ProductsHelper
 
-    def picture_and_name(record)    	
-      content_tag :div, :class => 'btn-group' do      	
-      	
-      	imgs = record.attachments.map do | a |
-        	image_tag(a.attachable.url("30x30"))
+    def picture_and_name(record)        
+      content_tag :div, :class => 'btn-group' do        
+        
+        imgs = record.attachments.map do | a |
+            image_tag(a.attachable.url("30x30"))
         end   
 
         imgs.join.html_safe + label_tag(record.name)
