8210ecc241f8c8e07a309da1202f9e75dd6129f1
kaka <huxinghai1988@gmail.com>
2013-01-28 16:21:03 +0800

    add columnview input

--- app/inputs/columnview_input.rb
+++ app/inputs/columnview_input.rb
@@ -1,15 +1,15 @@
 #encoding : utf-8
 class ColumnviewInput < SimpleForm::Inputs::Base
 
-  def input
+  def input    
     <<-HTML
         #{template.build_menu(@builder.object.send(attribute_name))} 
         #{@builder.hidden_field(attribute_name, input_html_options)}
         <script type="text/javascript">
-        $(document).ready(function(){
-          $("ul.dropdown-menu").first().columnview({field_name: '#{field_name}'});
-          $(".containerobj").hide();
-       });
+          require(["jquery_columnview"], function($){            
+            $("ul.dropdown-menu").first().columnview({field_name: '#{field_name}'});  
+            // $(".containerobj").hide();
+          })                
         </script>
     HTML
     .html_safe
