d0c15f07a294fb158fdbe3899e05e9c5e2e4e754
kaka <huxinghai1988@gmail.com>
2013-02-02 15:38:58 +0800

    fixed column view

--- app/inputs/columnview_input.rb
+++ app/inputs/columnview_input.rb
@@ -1,19 +1,25 @@
 #encoding : utf-8
 class ColumnviewInput < SimpleForm::Inputs::Base
 
-  def input    
+  def input
+    element_id = input_options[:id] || rand.to_s.split(".").last 
     <<-HTML
-        #{template.build_menu(@builder.object.send(attribute_name))} 
+        #{template.build_menu(input_options[:collection], element_id)} 
         #{@builder.hidden_field(attribute_name, input_html_options)}
         <script type="text/javascript">
           require(["jquery_columnview"], function($){            
-            $("ul.dropdown-menu").first().columnview({field_name: '#{field_name}'});  
-            // $(".containerobj").hide();
+            $("##{element_id}").columnview({
+                selector : function(data, current_data){                   
+                  $("input:hidden[name='#{@builder.object.class.name.underscore}[#{attribute_name}]']").val(current_data.id)                                                           
+                }
+            });  
           })                
         </script>
     HTML
     .html_safe
   end
+
+
 
   def field_name
     "#{lookup_model_names.join("_")}_#{reflection_or_attribute_name}"
