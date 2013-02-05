d16e0c6f9ebbbefe079177faf140e768094c6b92
qishao <290648797@qq.com>
2013-02-05 17:16:28 +0800

    first_commit

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,7 +1,14 @@
 class ProductItem
   include Mongoid::Document
 
-  attr_accessible :product, :title, :amount, :price
+  attr_accessible :product, 
+                  :title, 
+                  :amount, 
+                  :price, 
+                  :transaction, 
+                  :cart, 
+                  :product_id,
+                  :total
 
 
   field :title, type: String
@@ -21,13 +28,13 @@ class ProductItem
     product.preview.url("100x100")    
   end
 
-  after_create do |document|
-    cart = document.cart
-    cart.inc(:items_count, 1)
-  end
+  # after_create do |document|
+  #   cart = document.cart
+  #   cart.inc(:items_count, 1)
+  # end
 
-  after_destroy do |document|
-    cart = document.send
-    cart.inc(:items_count, -1)
-  end
+  # after_destroy do |document|
+  #   cart = document.send
+  #   cart.inc(:items_count, -1)
+  # end
 end
