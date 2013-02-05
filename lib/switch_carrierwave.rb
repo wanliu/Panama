cfd36c6f933010276dedbf49638cc836015b40a4
kaka <huxinghai1988@gmail.com>
2013-02-05 14:21:23 +0800

    fixed refactor switch uploader image

--- lib/switch_carrierwave.rb
+++ lib/switch_carrierwave.rb
@@ -14,31 +14,4 @@ module Carrierwave
     end
 end
 
-Carrierwave::Switch.configure do | storage |        
-    if storage == :file
-        ImageUploader.class_eval do        
-            include CarrierWave::MiniMagick         
-            def url(version_name = "")                                 
-                args = {}                
-                args = "t#{version_name.split("x").join}".to_sym unless version_name.blank?                
-                url = super(args)  
-                file_path = "#{ImageUploader.root.call}#{url}"
-                unless File.exist?(file_path)           
-                    return (version_name.blank? ? default_url : "#{default_url}!#{version_name}")
-                end
-
-                url            
-            end
-
-            CarrierWave::Uploader.version_names.each do | vname |            
-                xy = vname.split("x")
-                return if xy.length <= 0            
-                xy.push(xy[0]) if xy.length <= 1                                                
-
-                version "t#{vname.split("x").join}".to_sym do                
-                    process :resize_to_fill => xy.map{ | x | x.to_i }
-                end
-            end        
-        end
-    end
-end
+Carrierwave::Switch.configure
