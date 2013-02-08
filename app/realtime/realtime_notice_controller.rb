84978673552e1b3ff7f626cdced6fac2d0a14870
hysios hu <hysios@gmail.com>
2013-02-08 18:04:43 +0800

    year end save

--- app/realtime/realtime_notice_controller.rb
+++ app/realtime/realtime_notice_controller.rb
@@ -1,6 +1,7 @@
 class RealtimeNoticeController < FayeRails::Controller
 
   observe Transaction, :after_create do |new_transaction|
+    debugger
     RealtimeNoticeController.publish '/notice/transactions', new_transaction.attributes
   end 
 end
