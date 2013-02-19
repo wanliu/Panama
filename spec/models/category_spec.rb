6006a07217a9bc5518267d831301e5164df21852
hysios hu <hysios@gmail.com>
2013-02-19 18:51:13 +0800

    add Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
+require 'rake'
+
+def rake 
+	rake = Rake::Application.new
+	Rake.application = rake
+	rake.init
+    rake.load_rakefile
+	rake
+end
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "db:seed" do
+  	before(:each) do 
+  		Category.destroy_all
+  	end
+
+  	it "#root" do
+	    Category.root.should be_nil
+  	end
+
+  	it "run load category task" do 
+		rake['db:seed'].invoke
+		Category.root.should be_a_kind_of(Category)
+		Category.root.should have_at_most(100).children 
+  	end
+  end
 end
