728555a80aec3a8543c876ffabc65543121667d6
kaka <huxinghai1988@gmail.com>
2013-02-21 14:52:19 +0800

    fixed product spec

--- spec/controllers/admins/shops/products_controller_spec.rb
+++ spec/controllers/admins/shops/products_controller_spec.rb
@@ -2,11 +2,13 @@
 require 'spec_helper'
 
 describe Admins::Shops::ProductsController do
+  let(:shop){ FactoryGirl.create(:shop, ) }
   let(:product){ FactoryGirl.create(:product) }
+  let(:current_shop){ {:shop_id => product.shop.name} }
 
   describe "GET 'index'" do
     it "获取所有产品信息" do
-      get 'index', {:shop_id => product.shop.name}, get_session
+      get 'index',current_shop , get_session
       response.should be_success
 
       assigns(:categories).should_not be_nil
@@ -27,12 +29,29 @@ describe Admins::Shops::ProductsController do
     end
   end
 
-  describe 'get new ' do
+  describe 'GET new ' do
 
-    it "显示产品添加" do
-      get 'new', {:shop_id => product.shop.name}, get_session
+    it "显示产品添加页面" do
+      get 'new',current_shop , get_session
       response.should be_success
       response.should render_template(:new)
+    end
+  end
+
+  describe 'POST create' do
+
+    it "添加产品信息" do
+      options = {
+        :product => {
+          :name => "某某产品",
+          :price => 1.5,
+          :summary => "简单说明一下",
+          :description => "描述产品",
+          :shop_id => product.shop.id
+        }
+      }
+
+      post 'create', options, get_session
     end
   end
 end
