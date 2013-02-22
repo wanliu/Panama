a41eda5142ea96cb5c3e64f29508b32cfa69613b
qishao <290648797@qq.com>
2013-02-22 16:07:32 +0800

    columnview update

--- app/inputs/columnview_input.rb
+++ app/inputs/columnview_input.rb
@@ -1,25 +1,42 @@
-#encoding : utf-8
 class ColumnviewInput < SimpleForm::Inputs::Base
 
   def input
-    element_id = input_options[:id] || rand.to_s.split(".").last     
+    element_id = input_options[:id] || rand.to_s.split(".").last
+    category = input_options[:value]
+    if category.nil?
+        @builder.hidden_field(attribute_name, input_html_options.merge(:value => input_options[:value] ? input_options[:value].id : "" ))
+    end
     <<-HTML
-        #{template.build_menu(input_options[:collection], element_id)} 
-        #{@builder.hidden_field(attribute_name, input_html_options.merge(:value => input_options[:value] ? input_options[:value].id : "" ))}
+        #{template.build_menu(input_options[:collection], element_id)}
         <script type="text/javascript">
-          require(["jquery_columnview"], function($){            
+        if("" != '#{category}' ){
+          require(["jquery_columnview"], function($){
             $("##{element_id}").columnview({
-                selector : function(data, current_data){                   
-                  $("input:hidden[name='#{@builder.object.class.name.underscore}[#{attribute_name}]']").val(current_data.id)                                                           
-                }
-            });  
-          })                
+                length: '#{category.ancestry_depth}',
+                ancestry: '#{category.ancestry}',
+                category_id: '#{category.id}',
+                ihid: '#{field_name}',
+                ihname: '#{@builder.object.class.name.underscore}[#{attribute_name}]'
+            });
+          })
+        } else {
+          require(["jquery_columnview"], function($){
+            $("##{element_id}").columnview({
+              length: '',
+              ancestry: '',
+              category_id: '',
+              ihid: '',
+              ihname: '',
+              selector : function(data, current_data){
+                $("input:hidden[name='#{@builder.object.class.name.underscore}[#{attribute_name}]']").val(current_data.id)
+              }
+            });
+          })
+        }
         </script>
     HTML
     .html_safe
   end
-
-
 
   def field_name
     "#{lookup_model_names.join("_")}_#{reflection_or_attribute_name}"
