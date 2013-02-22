30fda1e4ec478f94113ddf0d29ba18f9f686715e
kaka <huxinghai1988@gmail.com>
2013-02-22 15:50:21 +0800

    fixed shop model spec

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,20 @@
+#encoding: utf-8
+
 require 'spec_helper'
 
-describe Shop do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Shop, "商店模型" do
+
+	it{ should belong_to(:user) }
+	it{ should have_many(:contents) }
+	it{ should have_many(:products) }
+	it{ should have_many(:transactions) }
+	it{ should have_one(:category) }
+
+	it{ should validate_presence_of(:name) }
+	it{ should validate_uniqueness_of(:name) }
+
+	it "检查属性" do
+		s = Shop.new
+		s.should respond_to(:name)
+	end
 end
