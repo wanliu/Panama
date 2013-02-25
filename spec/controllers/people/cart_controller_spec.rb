916d9020c5710005649793b863f6de540e89f545
lshgood <liushaohong159@163.com>
2013-02-25 11:03:57 +0800

    cart rspec edit

--- spec/controllers/people/cart_controller_spec.rb
+++ spec/controllers/people/cart_controller_spec.rb
@@ -18,37 +18,39 @@ describe People::CartController do
 	let(:user) { session_obj[:user] }
 	let(:my_cart) { user.cart }
 
-	describe " get 'index '购物车" do
-		it "add cart product_item" do
-			cart = my_cart.items.create! product_item
-			get 'index', {:page => 1}, session_obj
-			assigns(:items).should eq([cart])
-		end
-	end
-
-	describe "post 'add_to_cart' 购物车  增加" do
-		it "add cart product_item" do
-			post 'add_to_cart', {:product_item => product_item }, session_obj
-			item = product_item
-			assigns(:item).total.should eq((item[:amount] * item[:price]))
+	describe " 购物车 " do
+		describe " get 'index' " do
+			it "add cart product_item" do
+				cart = my_cart.items.create! product_item
+				get 'index', {:page => 1}, session_obj
+				assigns(:items).should eq([cart])
+			end
 		end
 
-		it "updat product_item amount" do
-			my_cart.items.create! product_item
-			item = product_item
-			item[:cart_id] = my_cart.id
-			post 'add_to_cart', {:product_item => item }, session_obj
-			item = product_item
-			assigns(:item).amount.should eq((item[:amount] + item[:amount]))
-		end
-	end
+		describe "post 'add_to_cart' 购物车  增加" do
+			it "add cart product_item" do
+				post 'add_to_cart', {:product_item => product_item }, session_obj
+				item = product_item
+				assigns(:item).total.should eq((item[:amount] * item[:price]))
+			end
 
-	describe "post 'clear_list' 购物车 册除" do
-		it "clear_list" do
-			my_cart.items.create! product_item
-			my_cart.items.create! product_item  
-			post 'clear_list',{}, session_obj 
-			my_cart.items.length.should eq(0)
+			it "updat product_item amount" do
+				my_cart.items.create! product_item
+				item = product_item
+				item[:cart_id] = my_cart.id
+				post 'add_to_cart', {:product_item => item }, session_obj
+				item = product_item
+				assigns(:item).amount.should eq((item[:amount] + item[:amount]))
+			end
+		end
+
+		describe "post 'clear_list' 购物车 册除" do
+			it "clear_list" do
+				my_cart.items.create! product_item
+				my_cart.items.create! product_item  
+				post 'clear_list',{}, session_obj 
+				my_cart.items.length.should eq(0)
+			end
 		end
 	end
 
