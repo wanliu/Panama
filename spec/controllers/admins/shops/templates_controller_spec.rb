54878479c3e61f1162bcb1bd525b182ec51182a7
qishao <290648797@qq.com>
2013-02-25 13:39:10 +0800

    add template rspec

--- spec/controllers/admins/shops/templates_controller_spec.rb
+++ spec/controllers/admins/shops/templates_controller_spec.rb
@@ -1,5 +1,15 @@
+#encoding : utf-8
 require 'spec_helper'
-
 describe Admins::Shops::TemplatesController do
+  def valid_attributes
+    {  }
+  end
+
+  describe "GET new" do
+  	it "测试创建 template" do
+  		get :new, {}, get_session
+  		assigns(:template).should be_a_new(Admins::Shops::Templates)
+  	end
+  end
 
 end
