9fe7155a0a299bdf774cf92db418f879c72ff60f
hysios hu <hysios@gmail.com>
2013-02-25 14:19:16 +0800

    FIXED: #6 app/controllers/shops_controller.rb

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,7 +4,7 @@ class Shop < ActiveRecord::Base
   include Graphical::Display
   extend FriendlyId
 
-  attr_accessible :name, :user
+  attr_accessible :name, :user_id, :user
 
   has_many :contents, dependent: :destroy
   has_many :products, dependent: :destroy
