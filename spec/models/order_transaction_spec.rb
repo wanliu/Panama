6255e4c84b26d350d2e8c0f84cb6f4913fddb645
kaka <huxinghai1988@gmail.com>
2013-02-25 18:22:22 +0800

    FIXED: app/spec/controllers/admins/shops/products_controller_spec.rb #6

--- spec/models/order_transaction_spec.rb
+++ spec/models/order_transaction_spec.rb
@@ -10,4 +10,15 @@ describe OrderTransaction, "订单流通记录" do
 
 	it{ should validate_presence_of(:buyer) }
 	it{ should validate_presence_of(:seller) }
+
+	it "检查属性" do
+		transaction = OrderTransaction.new
+		transaction.should response_to(:items_count)
+		transaction.should response_to(:total)
+		transaction.should response_to(:state)
+	end
+
+	it "数据验证" do
+
+	end
 end
