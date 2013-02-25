02a323467f35be2efbe433f6b8fb4e9ec200e6e6
lshgood <liushaohong159@163.com>
2013-02-25 17:44:01 +0800

    FIXED: spec/model/address_spec.rb, spec/model/cart_spec.rb #6

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,15 @@
+#encoding: utf-8
 require 'spec_helper'
 
-describe City do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe City, "城市地址" do
+
+    # describe "关联检查" do  
+	   #  it { should has_one(:address) } 
+    # end
+    
+    describe "属性验证" do 
+    	let(:city) { City.new }
+	    it { city.should respond_to(:name) } 
+    end
+    
 end
--- spec/models/product_item_spec.rb
+++ spec/models/product_item_spec.rb
@@ -1,5 +1,23 @@
+#encoding: utf-8
 require 'spec_helper'
 
-describe ProductItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe ProductItem, "购物车 商品 " do
+
+  	describe "关联检查" do 
+	    it { should belong_to(:cart) }
+	    it { should belong_to(:product) }
+	    it { should belong_to(:transaction).class_name("OrderTransaction") } 
+    end
+    
+    describe "属性验证" do 
+    	let(:product_item) { ProductItem.new }
+	    it { product_item.should respond_to(:amount) }
+	    it { product_item.should respond_to(:price) }
+	    it { product_item.should respond_to(:product_id) }
+	    it { product_item.should respond_to(:title) }
+	    it { product_item.should respond_to(:total) }
+	    it { product_item.should respond_to(:transaction_id) }
+	    it { product_item.should respond_to(:cart_id) } 
+    end
+    
 end
