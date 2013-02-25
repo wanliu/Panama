0d0d1224d6f1380d613b45189dbef975081e36e3
lshgood <liushaohong159@163.com>
2013-02-25 10:58:39 +0800

    cart rspec edit

--- spec/controllers/people/cart_controller_spec.rb
+++ spec/controllers/people/cart_controller_spec.rb
@@ -13,17 +13,20 @@ describe People::CartController do
 			:cart_id => '1'
 		}
 	end
- 
-	describe "post 'add_to_cart' 购物车  增加" do
-		let(:session_obj) { get_session }
-		let(:user) { session_obj[:user] }
-		let(:my_cart) { user.cart }
 
+	let(:session_obj) { get_session }
+	let(:user) { session_obj[:user] }
+	let(:my_cart) { user.cart }
+
+	describe " get 'index '购物车" do
 		it "add cart product_item" do
-			get 'index', {:page => 1}, session_obj 
-			assigns(:items) 
+			cart = my_cart.items.create! product_item
+			get 'index', {:page => 1}, session_obj
+			assigns(:items).should eq([cart])
 		end
+	end
 
+	describe "post 'add_to_cart' 购物车  增加" do
 		it "add cart product_item" do
 			post 'add_to_cart', {:product_item => product_item }, session_obj
 			item = product_item
@@ -38,8 +41,10 @@ describe People::CartController do
 			item = product_item
 			assigns(:item).amount.should eq((item[:amount] + item[:amount]))
 		end
+	end
 
-		it "add cart product_item updat product_item amount" do
+	describe "post 'clear_list' 购物车 册除" do
+		it "clear_list" do
 			my_cart.items.create! product_item
 			my_cart.items.create! product_item  
 			post 'clear_list',{}, session_obj 
