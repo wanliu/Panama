b01a20fb1fc8deb23849abf774bc62f0a8320f32
kaka <huxinghai1988@gmail.com>
2013-02-25 15:55:07 +0800

    FIXED: app/spec/controllers/admins/shops/products_controller_spec.rb #6

--- spec/requests/people/people_transactions_spec.rb
+++ spec/requests/people/people_transactions_spec.rb
@@ -4,7 +4,7 @@ describe "People::Transactions" do
   describe "GET /people_transactions" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get people_transactions_path
+      get people_transactions_path, get_session
       response.status.should be(200)
     end
   end
