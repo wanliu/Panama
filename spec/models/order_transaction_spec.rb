0da55f1d1aff4ce0ac6aa791997626a3619a9dc3
kaka <huxinghai1988@gmail.com>
2013-02-25 18:17:21 +0800

    FIXED: app/spec/controllers/admins/shops/products_controller_spec.rb

--- spec/models/order_transaction_spec.rb
+++ spec/models/order_transaction_spec.rb
@@ -1,5 +1,13 @@
+#encoding: utf-8
 require 'spec_helper'
 
-describe OrderTransaction do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe OrderTransaction, "订单流通记录" do
+
+	it{ should have_one(:address) }
+	it{ should belong_to(:seller) }
+	it{ should belong_to(:buyer) }
+	it{ should have_many(:items) }
+
+	it{ should validate_presence_of(:buyer) }
+	it{ should validate_presence_of(:seller) }
 end
