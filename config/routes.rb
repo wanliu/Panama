98fa7fba675d09012206439c55f155cb19865fe8
hysios hu <hysios@gmail.com>
2013-02-25 15:52:04 +0800

    fixed transactions routing FIXED: spec/routing/people/transactions_routing_spec.rb #6

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Panama::Application.routes.draw do
     end
   end
 
-  resources :people, :key => :login do
+  resources :people do
     resources :cart, :controller => "people/cart"
     resources :transactions, :controller => "people/transactions" do
       member do
