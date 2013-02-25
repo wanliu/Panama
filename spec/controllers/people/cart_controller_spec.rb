f141702d7dbad22eaa1fc1a2e52b39a4d039b359
hysios hu <hysios@gmail.com>
2013-02-25 11:18:37 +0800

    fixed conflict master

--- spec/controllers/people/cart_controller_spec.rb
+++ spec/controllers/people/cart_controller_spec.rb
@@ -1,10 +1,10 @@
 #encoding: utf-8
-require 'spec_helper' 
+require 'spec_helper'
 
 describe People::CartController do
 
 	def product_item
-		{ 
+		{
 			:title =>  "1111",
 			:amount => 11,
 			:price => 11,
@@ -13,37 +13,44 @@ describe People::CartController do
 			:cart_id => '1'
 		}
 	end
- 
-	describe "post 'add_to_cart' 购物车  增加" do
-		let(:session_obj) { get_session }
-		let(:user) { session_obj[:user] }
-		let(:my_cart) { user.cart }
 
-		it "add cart product_item" do
-			get 'index', {:page => 1}, session_obj 
-			assigns(:items) 
+	let(:session_obj) { get_session }
+	let(:user) { session_obj[:user] }
+	let(:my_cart) { user.cart }
+
+	describe " 购物车 " do
+		describe " get 'index' " do
+			it "add cart product_item" do
+				cart = my_cart.items.create! product_item
+				get 'index', {:page => 1}, session_obj
+				assigns(:items).should eq([cart])
+			end
 		end
 
-		it "add cart product_item" do
-			post 'add_to_cart', {:product_item => product_item }, session_obj
-			item = product_item
-			assigns(:item).total.should eq((item[:amount] * item[:price]))
+		describe "post 'add_to_cart' 购物车  增加" do
+			it "add cart product_item" do
+				post 'add_to_cart', {:product_item => product_item }, session_obj
+				item = product_item
+				assigns(:item).total.should eq((item[:amount] * item[:price]))
+			end
+
+			it "updat product_item amount" do
+				my_cart.items.create! product_item
+				item = product_item
+				item[:cart_id] = my_cart.id
+				post 'add_to_cart', {:product_item => item }, session_obj
+				item = product_item
+				assigns(:item).amount.should eq((item[:amount] + item[:amount]))
+			end
 		end
 
-		it "updat product_item amount" do
-			my_cart.items.create! product_item
-			item = product_item
-			item[:cart_id] = my_cart.id
-			post 'add_to_cart', {:product_item => item }, session_obj
-			item = product_item
-			assigns(:item).amount.should eq((item[:amount] + item[:amount]))
-		end
-
-		it "add cart product_item updat product_item amount" do
-			my_cart.items.create! product_item
-			my_cart.items.create! product_item  
-			post 'clear_list',{}, session_obj 
-			my_cart.items.length.should eq(0)
+		describe "post 'clear_list' 购物车 册除" do
+			it "clear_list" do
+				my_cart.items.create! product_item
+				my_cart.items.create! product_item
+				post 'clear_list',{}, session_obj
+				my_cart.items.length.should eq(0)
+			end
 		end
 	end
 
