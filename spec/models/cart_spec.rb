673f124689f7de8ae6d021be9810aab0242173b8
rakeLL <satbirdd@gmail.com>
2013-02-26 16:44:37 +0800

    rspec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,15 +3,27 @@ require 'spec_helper'
 
 describe Cart, "model 购物车 " do
 
-	describe "关联检查" do 
-	    it { should belong_to(:user) }
-	    it { should have_many(:items).class_name('ProductItem') } 
-    end
-    
-    describe "属性验证" do 
-    	let(:cart) { Cart.new }
-	    it { cart.should respond_to(:items_count) }
-	    it { cart.should respond_to(:user_id) }
-    end
-    
+	describe "关联检查" do
+    it { should belong_to(:user) }
+    it { should have_many(:items).class_name('ProductItem') }
+  end
+
+  describe "属性验证" do
+  	let(:cart) { Cart.new }
+    it { cart.should respond_to(:items_count) }
+    it { cart.should respond_to(:user_id) }
+  end
+
+  describe "实例方法" do
+  	describe "create_transaction" do
+  		
+  	end
+  	describe "shop_items" do
+  		
+  	end
+  	describe "save_transcation" do
+  		
+  	end
+  end
+
 end
