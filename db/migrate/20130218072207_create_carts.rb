f21f76631cb282d56f5c6cdc0396f89b1ea6ffff
hysios hu <hysios@gmail.com>
2013-02-26 17:45:28 +0800

    修复一个 user session restore 诡异 bug

--- db/migrate/20130218072207_create_carts.rb
+++ db/migrate/20130218072207_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
-      t.integer :items_count
+      t.integer :items_count, :default => 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -58,9 +58,9 @@ ActiveRecord::Schema.define(:version => 20130223095848) do
   end
 
   create_table "carts", :force => true do |t|
-    t.integer  "items_count"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer  "items_count", :default => 0
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "user_id"
   end
 
