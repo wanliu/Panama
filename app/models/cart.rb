ebe2ecae297e8151cf6c19f7b64934f8bccbbbbb
rakeLL <satbirdd@gmail.com>
2013-02-27 10:16:26 +0800

    change test

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,7 @@ class Cart < ActiveRecord::Base
   has_many :items, inverse_of: :cart, class_name: 'ProductItem', autosave: true  
 
   def create_transaction(people)
-  	done = shop_items.map { |shop, items| save_transcation(shop, items, people) }.all?
+  	done = shop_items.map { |shop, pro_items| save_transcation(shop, pro_items, people) }.all?
   	destroy if done # FIXME :should't be items.clear?
   	done
   end
@@ -14,11 +14,15 @@ class Cart < ActiveRecord::Base
   	items.group_by { |item| item.product.shop }
   end
 
-  def save_transcation(shop, items, people)
-  	transaction = people.transactions.build seller_id: shop.id
-    items.each {|item| transaction.items.build item.attributes }
-    transaction.items_count = items.inject(0) { |s, item| s + item.amount }
-    transaction.total = items.inject(0) { |s, item| s + item.total }
-    transaction.save
+  def save_transcation(shop, pro_items, people)
+  	transaction = people.transactions.create(seller_id: shop.id)
+  	transaction.create_items(pro_items)
+  	transaction.update_total_count
+
+  	# transaction = people.transactions.build seller_id: shop.id
+   #  items.each {|item| transaction.items.build item.attributes }
+   #  transaction.items_count = items.inject(0) { |s, item| s + item.amount }
+   #  transaction.total = items.inject(0) { |s, item| s + item.total }
+   #  transaction.save
   end
 end
