c242a304a3364ae09865dcad84837b4d9af2e752
lshgood <liushaohong159@163.com>
2013-02-27 18:29:48 +0800

    decorator gem add

---
+++ app/decorators/product_item_decorator.rb
@@ -0,0 +1,30 @@
+class ProductItemDecorator < Draper::Decorator
+
+  def price
+    h.number_to_currency(source.price).delete("CN")
+  end
+
+  def total 
+    h.number_to_currency(source.total).delete("CN")
+  end
+
+  # def renminbi(m)
+  #   m = format("%.2f",m).to_f.to_s
+  #   left = ""
+  #   if m.to_s[0] == "-"
+  #     m = m[1,m.length]
+  #     left = "-"
+  #   end
+  #   a = m.split(".")
+  #   l = a[0].reverse() 
+  #   r = a[1] 
+  #   t = ""
+  #   i = 0
+  #   l.length.times do
+  #     t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "")
+  #     i+=1
+  #   end
+  #   left + t.reverse() + "." + r
+  # end
+
+end
