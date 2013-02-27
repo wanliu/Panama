bfeac5b65157c9230366bab0ff5f70b114c3c3e0
kaka <huxinghai1988@gmail.com>
2013-02-27 11:50:07 +0800

    fixed table  comments

---
+++ app/controllers/people/comments_controller.rb
@@ -0,0 +1,37 @@
+class People::Comments < People::BaseController
+
+    def index
+        @comments = Comment.where(:product_id => params[:product_id])
+    end
+
+    def show
+        @comment = Comment.find(params[:id])
+    end
+
+    def new
+        @comment = Comment.new
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    def create
+        @comment = Comment.new(params[:comment])
+        if @product_comment.save
+            render :action => :show
+        else
+            render :action => :edit
+        end
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        @comment.update_attribute(params[:comment])
+        if @comment.save
+            render :action => :show
+        else
+            render :action => :edit
+        end
+    end
+end
