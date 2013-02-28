3c6e467cbc79f55937af10f6834dbdc16a58ef72
rakeLL <satbirdd@gmail.com>
2013-02-28 16:12:54 +0800

    modify the product_item model

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,14 +1,20 @@
 class ProductItem < ActiveRecord::Base
-  attr_accessible :amount, :price, :product_id, :title, :total, :transaction_id, :cart
+  attr_accessible :amount, :price, :sub_product_id, :title, :total, :transaction_id, :cart
 
   belongs_to :cart, inverse_of: :items, :counter_cache => :items_count
-  belongs_to :product
+  belongs_to :sub_product
   belongs_to :transaction,
              class_name: "OrderTransaction",
              primary_key: 'transaction_id'
 
   delegate :photos, :to => :product
   delegate :icon, :header, :avatar, :preview, :to => :photos
+
+  validates :sub_product_id, presence: true
+
+  def product
+    sub_product.product
+  end
 
   # after_save do |item|
   #   debugger
