ad476010684fd1fb42752a0aa7fdd77a705273bd
kaka <huxinghai1988@gmail.com>
2013-03-06 17:33:12 +0800

    fixed

--- app/realtime/realtime_notice_controller.rb
+++ app/realtime/realtime_notice_controller.rb
@@ -1,6 +1,6 @@
 class RealtimeNoticeController < FayeRails::Controller
     observe Notification, :after_create do |notice|
-    	puts "notice: #{notice.user.login}"
+        puts "notice: #{notice.user.login}"
         RealtimeNoticeController.publish("/notification/#{notice.user.login}", notice.attributes)
     end
 end
