3eea28676308e1358ae0aadd19e2b14b17212925
rakeLL <satbirdd@gmail.com>
2013-03-07 16:32:37 +0800

    add item form

--- app/admin/property.rb
+++ app/admin/property.rb
@@ -3,12 +3,27 @@ ActiveAdmin.register Property do
     column :title
     column :name
     column :property_type
-    column :items do |property|
-      link_to "Has #{property.items.size} items",items_system_property_path(property)
-    end
+    # column :items do |property|
+    #   link_to "Has #{property.items.size} items",items_system_property_path(property)
+    # end
     # column :user
+    default_actions
   end
 
   member_action :items do
   end
+
+  # form do |f|
+  #   f.inputs do
+  #     f.input :title
+  #     f.input :name
+  #     f.input :property_type, :as => :select, :collection => %w(string set datetime integer float decimal)
+  #     f.semantic_fields_for :items do |items|
+  #       items.inputs
+  #       # items.buttons
+  #     end
+  #   end
+
+  #   f.buttons
+  # end
 end
