5e2aca9e6c8e18e03d33a4c32b8e316d485c965d
rakeLL <satbirdd@gmail.com>
2013-03-08 16:21:51 +0800

    add properties links to product

--- app/admin/admin_product.rb
+++ app/admin/admin_product.rb
@@ -2,5 +2,8 @@ ActiveAdmin.register Product do
   index do
     column :id
     column :name
+    column :properties do |product|
+    	link_to "Has #{ product.properties.size } properties", properties_system_category_path(product.category)
+    end
   end
 end
