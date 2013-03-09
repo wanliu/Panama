a01a0eae43acf0dcea978803279a241c25cbc985
qishao <290648797@qq.com>
2013-03-09 15:17:30 +0800

    comment update

--- app/controllers/people/comments_controller.rb
+++ app/controllers/people/comments_controller.rb
@@ -1,22 +1,25 @@
 class People::CommentsController < People::BaseController
 
-    def index
+    def index_activities
         @activities = Activity.all
+    end
+
+    def index
+        @comments = Comment.where("targeable_id=? and targeable_type=?", 
+            params[:targeable_id], 
+            params[:targeable_type])
+        if "0" != params[:limit]
+            @comments = @comments.order("created_at desc").limit(params[:limit]).reverse()
+        end
+        @activity = Activity.find(params[:targeable_id])
         @comment = Comment.new
-        @comments = Comment.where("targeable_type=? and targeable_id=?",
-            params[:targeable_type], params[:targeable_id])
+        respond_to do | format |
+            format.html{ render :layout => false }
+        end
     end
 
     def show
         @comment = Comment.find(params[:id])
-    end
-
-    def new_activity
-        @comment = Comment.new
-    end
-
-    def new_product
-        @comment = Comment.new
     end
 
     def edit
@@ -26,10 +29,19 @@ class People::CommentsController < People::BaseController
     #活动评论
     def activity
         @comment = Comment.activity(params[:comment].merge(:user_id => current_user.id))
-        if @comment.valid?
-            render :action => :show
-        else
-            render :action => :edit
+        users = @comment.content_extract_users
+        users.each do |user|
+            @comment.content = @comment.content.gsub(/@#{user.login}/,
+                "<a href='#'>@#{user.login}</a>")
+        end
+        @comment.save
+        respond_to do |format|
+            if @comment.valid?
+                format.html { render :action => :show }
+                format.js { render :json => @comment.as_json.merge(@comment.user.as_json) }
+            else
+                render :action => :edit
+            end
         end
     end
 
