fcc94cdd8b2c003bceed0862ad10ce044d7ab554
lshgood <liushaohong159@163.com>
2013-03-09 13:40:26 +0800

    vfs

---
+++ app/admin/deploy_file.rb
@@ -0,0 +1,30 @@
+#encoding: utf-8
+
+# ActiveAdmin.register FileEntity, :as => 'VFS' do
+
+#   index do 
+#     @root = '/'.to_dir 
+#     render "index", :root => @root['*']
+#   end
+
+# end
+
+
+ActiveAdmin.register_page "VFS" do
+  # menu :label => "My Menu Item Label", :parent => "Dashboard"
+
+  sidebar :所有条件 do
+    ul do
+      li "没有可用条件" 
+    end
+  end
+
+  action_item do
+    link_to "重置", "/system/vfs"
+  end
+
+  content do
+    @root = '/'.to_dir 
+    render "index", :root => @root['*']
+  end
+end
