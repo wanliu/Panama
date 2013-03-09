c1450ec7e94ad58a31328184f489fea9ad531901
lshgood <liushaohong159@163.com>
2013-03-09 18:26:32 +0800

    vfs

---
+++ app/controllers/vfs_controller.rb
@@ -0,0 +1,18 @@
+class VfsController < ApplicationController
+	def show  
+		root = '/'.to_dir
+		json_obj = z_json root["/_shops#{params[:p]}"]['*'], params[:file_path]
+		render :json => json_obj
+	end
+  
+	def z_json(root, file_path)  
+		obj_a = {p: file_path}
+		obj_a[:n] = []
+		root.each do |r|
+			obj_a[:n] << {
+				name: r.name
+			}
+		end
+		obj_a
+	end
+end
