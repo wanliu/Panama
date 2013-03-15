a864eb04481061cae625789cc55d53199d7566d2
lshgood <liushaohong159@163.com>
2013-03-15 15:16:45 +0800

    vfs create remove select edit ok

--- app/models/app_file.rb
+++ app/models/app_file.rb
@@ -1,34 +1,10 @@
 class AppFile
-
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  
   attr_accessor :name, :data
   
-  # def data=(value)
-  #   write_attribute(:data, value)
-  # end
- 
-end
-
-
-class FileLoad
-
-  cattr_accessor :shop
-
-  attr_accessor  :data
-
-  def initialize(file_path = nil)
-    debugger
-    unless file_path.blank?
-      @file_path = file_path      
-      fs = '/'.to_dir
-      @fs = fs[@file_path]
-    end
+  def persisted?
+    false
   end
-
-  def data
-    @fs.read
-  end
-
-  def data=(value)
-    @fs.write(value)
-  end
-end
+end 
