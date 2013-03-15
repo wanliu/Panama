5257dd32eec21336f2eabdeb03b5df17a49d9ece
kaka <huxinghai1988@gmail.com>
2013-03-15 10:18:55 +0800

    fixed permission

--- app/models/shop_user.rb
+++ app/models/shop_user.rb
@@ -5,4 +5,12 @@ class ShopUser < ActiveRecord::Base
   belongs_to :shop
 
   has_many :shop_user_groups, :dependent => :destroy
+
+  def groups
+  	shop_user_groups.map{|g| g.shop_group }
+  end
+
+  def jshop
+  	shop
+  end
 end
