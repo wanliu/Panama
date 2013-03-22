fdf72a6890cbf939772c473f97d7a12c95c8b3d7
kaka <huxinghai1988@gmail.com>
2013-03-22 11:01:35 +0800

    fixed following controller permission

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -11,6 +11,7 @@ class Shop < ActiveRecord::Base
   has_many :groups, dependent: :destroy, class_name: "ShopGroup"
   has_many :transactions, class_name: "OrderTransaction", :foreign_key => "seller_id"
   has_many :shop_users
+  has_many :followings, :as => :follow
 
   has_one :shops_category
   belongs_to :user
