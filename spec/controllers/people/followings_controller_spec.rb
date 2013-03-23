67ba5f40571f11f5b7687461b1889a18507f4b4a
kaka <huxinghai1988@gmail.com>
2013-03-23 10:00:05 +0800

    fixed following view

--- spec/controllers/people/followings_controller_spec.rb
+++ spec/controllers/people/followings_controller_spec.rb
@@ -5,62 +5,76 @@ require "spec_helper"
 
 describe People::FollowingsController, "关注控制器" do
 
-	let(:shop){ FactoryGirl.create(:shop, user: anonymous) }
+  let(:shop){ FactoryGirl.create(:shop, user: anonymous) }
 
-	def person_opts
-		{person_id: current_user.login}
-	end
+  def person_opts
+    {person_id: current_user.login}
+  end
 
-	describe "GET index 所有关注" do
-		before :each do
+  describe "GET index 所有关注" do
+    before :each do
       @fuser = current_user.followings.user(anonymous.id)
       @fshop = current_user.followings.shop(shop.id)
-		end
+    end
 
-		it "获取所有" do
-			get :index, person_opts, get_session
+    it "获取所有" do
+      get :index, person_opts, get_session
       response.should be_success
-			assigns(:followings).should eq([@fuser, @fshop])
-		end
+      assigns(:u_followings).should eq([@fuser])
+      assigns(:s_followings).should eq([@fshop])
+    end
 
-		it "查看别人的关注" do
-			get :index, person_opts
-      assigns(:followings).should eq([@fuser, @fshop])
-		end
-	end
+    it "查看别人的关注" do
+      get :index, person_opts
+      assigns(:u_followings).should eq([@fuser])
+      assigns(:s_followings).should eq([@fshop])
+    end
+  end
 
   describe "post user 关注用户" do
 
     it "变更数据" do
       expect{
-        xhr :post, :user, person_opts.merge(user_id: anonymous.id), get_session
+        post :user, person_opts.merge(user_id: anonymous.id, format: :json), get_session
         }.to change(Following, :count).by(1)
     end
 
     it "成功" do
       user = anonymous
-      xhr :post, :user, person_opts.merge(user_id: user.id), get_session
-      assigns(:following).user.should eq(current_user)
-      assigns(:following).follow.should eq(user)
-    end
-
-    it "失败" do
-      xhr :post, :user, person_opts.merge, get_session
-      response.response_code.should eq(403)
+      post :user, person_opts.merge(user_id: user.id, format: :json), get_session
+      assigns(:follow).user.should eq(current_user)
+      assigns(:follow).follow.should eq(user)
     end
   end
 
   describe "post shop 关注商店" do
 
     it "成功" do
-      xhr :post, :shop, person_opts.merge(shop_id: shop.id), get_session
-      assigns(:following).user.should eq(current_user)
-      assigns(:following).follow.should eq(shop)
+      post :shop, person_opts.merge(shop_id: shop.id, format: :json), get_session
+      assigns(:follow).user.should eq(current_user)
+      assigns(:follow).follow.should eq(shop)
+    end
+  end
+
+  describe "delete destroy 取消关注" do
+
+    it "用户" do
+      follow = current_user.followings.user(anonymous.id)
+      delete :destroy, person_opts.merge(id: follow.id, format: :json), get_session
+      assigns(:follow).should eq(follow)
+      Following.find_by(id: follow.id).should be_nil
     end
 
-    it "失败" do
-      xhr :post, :shop, person_opts, get_session
-      response.response_code.should eq(403)
+    it "商店" do
+      follow = current_user.followings.shop(shop.id)
+      delete :destroy, person_opts.merge(id: follow.id, format: :json), get_session
+      assigns(:follow).should eq(follow)
+      Following.find_by(id: follow.id).should be_nil
     end
+
+  end
+
+  describe "get followers 关注我的" do
+
   end
 end
