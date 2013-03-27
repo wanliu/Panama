c9566d99b5c2da8f133e57e21665b71279b20a8f
kaka <huxinghai1988@gmail.com>
2013-03-27 17:10:52 +0800

    fixed topic

--- app/models/topic_receive.rb
+++ app/models/topic_receive.rb
@@ -2,16 +2,31 @@
 # describe: 帖子接收者
 # attributes:
 #   topic_id: 帖子
-#   user_id: 接收者(用户)
+#   receive_id: 接收者(用户或者商店)
 class TopicReceive < ActiveRecord::Base
-  attr_accessible :user_id, :topic_id
+  attr_accessible :receive_id, :topic_id, :receive_type
 
   belongs_to :topic
-  belongs_to :user
+  belongs_to :receive, :polymorphic => true
 
   validates :topic_id, presence: true
-  validates :user_id, presence: true
+  validates :receive_id, presence: true
+  validates :receive_type, presence: true
 
-  validates_presence_of :user
+  validates_presence_of :receive
   validates_presence_of :topic
+
+  def self.user(user, topic_id = nil)
+    options = {receive_id: user, receive_type: "User"}
+    options[:receive_id] = user.id if user.is_a?(User)
+    options[:topic] = topic_id unless topic_id.nil?
+    create(options)
+  end
+
+  def self.shop(shop, topic_id = nil)
+    options = {receive_id: shop, receive_type: "Shop"}
+    options[:receive_id] = shop.id if shop.is_a?(Shop)
+    options[:topic] = topic_id unless topic_id.nil?
+    create(options)
+  end
 end
