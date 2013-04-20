5ff131233135ab62c31b63e507d21f83fe358d73
kaka <huxinghai1988@gmail.com>
2013-04-20 14:47:31 +0800

    fixed spec

--- app/models/topic_category.rb
+++ app/models/topic_category.rb
@@ -1,7 +1,18 @@
+#encoding: utf-8
 #descibe: 帖子类目
 class TopicCategory < ActiveRecord::Base
   attr_accessible :name, :shop_id
 
   belongs_to :shop
   validates :name, presence: true
+
+  validates_presence_of :shop
+
+  private
+  def valid_exists?
+    if TopicCategory.where("shop_id=? and name=? and id<>?",
+    shop_id, name, id.to_s).count > 0
+      errors.add(:name, "名称重复了！")
+    end
+  end
 end
--- spec/models/topic_category_spec.rb
+++ spec/models/topic_category_spec.rb
@@ -1,5 +1,21 @@
+#encoding: utf-8
 require 'spec_helper'
 
-describe TopicCategory do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe TopicCategory, "帖子分类模型" do
+  let(:shop){ FactoryGirl.create(:shop, user: current_user) }
+
+  it{ should belong_to :shop }
+  it{ should validate_presence_of :shop }
+
+  it "验证数据" do
+    @topic_category = TopicCategory.new(
+      shop_id: shop.id,
+      name: "服务支持")
+    @topic_category.valid?.should be_true
+
+    TopicCategory.create(
+      shop_id: shop.id,
+      name: "服务支持").valid?.should be_true
+  end
+
 end
