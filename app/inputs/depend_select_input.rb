7628d559a3797b4ccb032ef76766696e983ecc1f
lshgood <liushaohong159@163.com>
2013-04-23 10:51:43 +0800

    depend select edit ok

--- app/inputs/depend_select_input.rb
+++ app/inputs/depend_select_input.rb
@@ -3,11 +3,13 @@ class DependSelectInput < SimpleForm::Inputs::CollectionSelectInput
   def input
     isif = reflection.blank?
     strID = isif ? "" : "_id"
-    children = input_options[:children].blank? ? "" : children_name
+    children = input_options[:children].blank? ? "" : children_dom_id
     url = input_options[:collection_url] || ""
     @collection = input_options[:collection] ?  collection :  []
     
     label_method, value_method = detect_collection_methods
+    
+    input_html_options[:class].push(dom_id) 
     @builder.collection_select(
       attribute_name, @collection, value_method, label_method,
       input_options, input_html_options
@@ -15,7 +17,7 @@ class DependSelectInput < SimpleForm::Inputs::CollectionSelectInput
     <<-JAVASCRIPT
       <SCRIPT type='text/javascript'>
       	require(['jquery', 'lib/depend_select'], function($, DependSelectView){
-            new DependSelectView({el: '##{field_name+strID}', children: '#{children}',url: '#{url}'})
+            new DependSelectView({el: '.#{dom_id}', children: '#{children}',url: '#{url}'})
         });
       </SCRIPT>
     JAVASCRIPT
@@ -28,5 +30,15 @@ class DependSelectInput < SimpleForm::Inputs::CollectionSelectInput
 
   def children_name
     "#{lookup_model_names.join("_")}_#{input_options[:children]}"
+  end
+
+  def dom_id
+    return field_name if input_options[:object].blank?
+    @builder.template.dom_id(input_options[:object], field_name)
+  end
+
+  def children_dom_id 
+    return children_name if input_options[:object].blank? 
+    @builder.template.dom_id(input_options[:object], children_name)
   end
 end
