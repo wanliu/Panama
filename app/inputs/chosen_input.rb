ff8491b7eb1b3ce6668ffd43add550c919a93fc2
z2009zxiaolong <z2009zxiaolong@gmail.com>
2013-05-07 10:02:11 +0800

    support muti order addresses

--- app/inputs/chosen_input.rb
+++ app/inputs/chosen_input.rb
@@ -8,29 +8,36 @@ class ChosenInput < SimpleForm::Inputs::CollectionSelectInput
     remote_value = input_options[:remote_value] || "id"
     param_name = input_options[:param_name] || "q"
     label_method, value_method = detect_collection_methods
-
+    # input_html_options[:class].push(dom_id)
+    
     @builder.collection_select(
       attribute_name, collection, value_method, label_method,
       input_options, input_html_options
-    ) +
-    <<-JAVASCRIPT
-    <SCRIPT type='text/javascript'>
-    	require(['jquery', 'lib/chosen.ex'], function($){
-        $("##{field_name+@strID}").chosenEx({
-          remote: {
-            url: '#{input_options[:url]}',
-            remote_key: #{remote_key.inspect},
-            remote_value: #{remote_value.inspect},
-            param_name: #{param_name.inspect}
-          }
-        });
-      });
-    </SCRIPT>
-    JAVASCRIPT
-    .html_safe
+    ).html_safe 
+    # +
+    # <<-JAVASCRIPT
+    # <SCRIPT type='text/javascript'>
+    # 	require(['jquery', 'lib/chosen.ex'], function($){
+    #     $(".#{dom_id}").chosenEx({
+    #       remote: {
+    #         url: '#{input_options[:url]}',
+    #         remote_key: #{remote_key.inspect},
+    #         remote_value: #{remote_value.inspect},
+    #         param_name: #{param_name.inspect}
+    #       }
+    #     });
+    #   });
+    # </SCRIPT>
+    # JAVASCRIPT
+    # .html_safe
   end
 
   def field_name
   	"#{lookup_model_names.join("_")}_#{reflection_or_attribute_name}"
+  end
+
+  def dom_id
+    return field_name if input_options[:object].blank?
+    @builder.template.dom_id(input_options[:object], field_name)
   end
 end
