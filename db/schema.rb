28d5dbc5d5ea74235d41d20a22996200b49ca3aa
kaka <huxinghai1988@gmail.com>
2013-05-08 10:11:31 +0800

    add receive order message model validate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426083412) do
+ActiveRecord::Schema.define(:version => 20130508015854) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -385,6 +385,15 @@ ActiveRecord::Schema.define(:version => 20130426083412) do
     t.datetime "created_at",                                    :null => false
     t.datetime "updated_at",                                    :null => false
     t.string   "valuable_type"
+  end
+
+  create_table "receive_order_messages", :force => true do |t|
+    t.integer  "order_transaction_id"
+    t.integer  "send_user_id"
+    t.text     "message"
+    t.boolean  "state",                :default => false
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
   end
 
   create_table "replies", :force => true do |t|
