d5ee59df8eaf7b5a3fd55c3b38529d0c3711a2ba
z2009zxiaolong <z2009zxiaolong@gmail.com>
2013-05-10 16:53:54 +0800

    add new address to order

--- app/models/order_transaction.rb
+++ app/models/order_transaction.rb
@@ -5,8 +5,8 @@ class OrderTransaction < ActiveRecord::Base
   attr_accessible :buyer_id, :items_count, :seller_id, :state, :total, :address
   attr_accessor :total
 
-  has_one :address,
-          foreign_key: 'transaction_id'
+  belongs_to :address,
+          foreign_key: 'address_id'
 
   belongs_to :seller, class_name: "Shop"
   belongs_to :buyer,
