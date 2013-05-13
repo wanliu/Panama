0df9e7535bf135981dd50956ff3dc32cdaa00d5c
z2009zxiaolong <z2009zxiaolong@gmail.com>
2013-05-13 11:12:47 +0800

    modify progress bar

--- db/schema.rb
+++ db/schema.rb
@@ -88,9 +88,9 @@ ActiveRecord::Schema.define(:version => 20130511015644) do
   end
 
   create_table "carts", :force => true do |t|
-    t.integer  "items_count"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer  "items_count", :default => 0
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "user_id"
   end
 
@@ -278,8 +278,8 @@ ActiveRecord::Schema.define(:version => 20130511015644) do
     t.datetime "created_at",                                                         :null => false
     t.datetime "updated_at",                                                         :null => false
     t.integer  "address_id"
-    t.boolean  "operator_state",                                  :default => false
     t.integer  "delivery_type_id"
+    t.boolean  "operator_state",                                  :default => false
   end
 
   create_table "permissions", :force => true do |t|
