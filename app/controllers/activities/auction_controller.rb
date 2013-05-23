a7372714f67844189ef6ddd007f20b319634a5c0
hysios hu <hysios@gmail.com>
2013-05-23 15:23:01 +0800

    ajax load 页面

--- app/controllers/activities/auction_controller.rb
+++ app/controllers/activities/auction_controller.rb
@@ -1,6 +1,22 @@
 class Activities::AuctionController < ApplicationController
 
+  def new
+    @activity = Activity.new
+    @activity.extend(AuctionExtension)
+
+    respond_to do |format|
+      format.html { render :layout => false }
+    end
+  end
 
   def create
+
   end
+end
+
+
+module AuctionExtension
+
+  attr_accessor :price, :product
+
 end
