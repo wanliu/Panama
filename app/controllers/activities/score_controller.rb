39df93c954df0c6e9ec3b38169aebd2c2dd17828
hysios hu <hysios@gmail.com>
2013-05-24 15:30:15 +0800

    new simple_form input field

--- app/controllers/activities/score_controller.rb
+++ app/controllers/activities/score_controller.rb
@@ -8,15 +8,17 @@ class Activities::ScoreController < ApplicationController
 	    end
     end
 
-	 def create
-
+	def create
+		params
+		debugger
+		self
 	end
 end
 
 
 module ScoreExtension
 
-  attr_accessor :price, :product,:picture,:useableScore,:returnScore,:begin_time,:end_time
+  attr_accessor :price, :product, :give_score, :limit_score, :picture, :useableScore, :returnScore, :begin_time, :end_time
 
 end
 
