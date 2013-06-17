e916d9421f0c4ebcc0ec5b22e93b07051c411381
kaka <huxinghai1988@gmail.com>
2013-06-17 11:37:53 +0800

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130616024808) do
+ActiveRecord::Schema.define(:version => 20130617033132) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -489,6 +489,7 @@ ActiveRecord::Schema.define(:version => 20130616024808) do
     t.integer  "category_id"
     t.integer  "default_attachment_id"
     t.integer  "shops_category_id"
+    t.string   "brand_name"
   end
 
   create_table "products_properties", :force => true do |t|
