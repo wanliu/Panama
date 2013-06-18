f702c99eeddd123b0545ee6e7803e3035c13f062
kaka <huxinghai1988@gmail.com>
2013-06-18 18:21:05 +0800

    fixed

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,6 @@
 module ProductsHelper
 
-	def dispose_options(product)
+  def dispose_options(product)
     attachments = product.fetch(:attachment_ids, {})
     {:attachment_ids => attachments.map{|k, v| v} }
   end
