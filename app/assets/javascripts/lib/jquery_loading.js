a1e943ff8210b39c9dca0777e692559322616c67
kaka <huxinghai1988@gmail.com>
2013-06-18 13:58:41 +0800

    fixed

---
+++ app/assets/javascripts/lib/jquery_loading.js
@@ -0,0 +1,2 @@
+//= require ./jquery.loading
+//= require ./extend_loading
--- app/assets/javascripts/panama.js
+++ app/assets/javascripts/panama.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require lib/jquery_loading
 //= require lib/jquery.raty
 //= require lib/underscore
 //= require backbone
