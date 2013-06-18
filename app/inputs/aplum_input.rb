ab14271c9bec48402327bff7e3a6cf3d7bfc1c19
kaka <huxinghai1988@gmail.com>
2013-06-18 16:59:19 +0800

    fixed

--- app/inputs/aplum_input.rb
+++ app/inputs/aplum_input.rb
@@ -4,49 +4,48 @@ class AplumInput
   include Formtastic::Inputs::Base
 
   def to_html
-    main_template
-  end
-
-  private
-  def main_template
     collection = options[:collection] || []
-    upload_url = options[:upload_url]
+    upload_url = options[:upload_url] || template.attachments_path
     img_version = options[:img_version]
     default_url = options[:default_url] || template.default_img_url(img_version)
     el_id = rand.to_s.gsub(".", "")
 
-    output = ActiveSupport::SafeBuffer.new
-    output << template.content_tag(:li) do
-      template.content_tag(:label, "图片", class: "label") +
-      template.content_tag(:div,nil, id: el_id)
+    input_wrapping do
+      output = ActiveSupport::SafeBuffer.new
+      output << template.content_tag(:li) do
+        template.content_tag(:label, "图片", class: "label") +
+        template.content_tag(:ul,nil, id: el_id, class: "attachment-list")
+      end
+
+      output << template.javascript_tag(<<-JAVASCRIPT
+        $(document).ready(function(){
+          new ProductUpload({
+            el: $("##{el_id}"),
+            data: #{collection.to_json},
+            params: {
+              url_upload: "#{upload_url}" ,
+              default_img_url: "#{default_url}",
+              template: '#{template.j(upload_template)}',
+              version_name: "#{img_version}",
+              input_name: "product[attachment_ids]",
+              default_input_name: "product[default_attachment_id]"
+            }
+          })
+        })
+      JAVASCRIPT
+      )
+      output
     end
-
-    output << template.javascript_tag(<<-JAVASCRIPT
-      new ProductUpload({
-        el: document.getElementById("#{el_id}"),
-        data: #{collection.to_json},
-        params: {
-          url_upload: "#{upload_url}" ,
-          default_img_url: "#{default_url}",
-          template: '#{template.j(upload_template)}',
-          version_name: "#{img_version}",
-          input_name: "product[attachment_ids]",
-          default_input_name: "product[default_attachment_id]"
-        }
-      })
-    JAVASCRIPT
-    )
-
-    output
   end
 
+  private
   def upload_template
     <<-HTML
     <div class="attachable">
       <img  class="img-rounded attachable-preview" />
       <input type="hidden" />
       <div class="attachment-upload"></div>
-      <div class="operation-panle">
+      <div class="nav-panle">
         <a href="javascript:void(0)" class="delete-img">删除</a>
         <a href="javascript:void(0)" class="default-index-img">设为主图</a>
       </div>
