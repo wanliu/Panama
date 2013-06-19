2a8d3a06530d8610346fbadbe46524e32dda913a
xifengzhu <xifengzhu520@gmail.com>
2013-06-19 17:03:29 +0800

    temp

--- app/admin/admin_product.rb
+++ app/admin/admin_product.rb
@@ -10,6 +10,7 @@ ActiveAdmin.register Product do
 
   index do
 
+
     column :name
     column :properties do |product|
       properties_string = product.properties.map do |prop|
@@ -29,20 +30,8 @@ ActiveAdmin.register Product do
       panel("Product Base") do
         attributes_table_for(product) do
           attrbute_names = product.attributes.map { |attr, _| attr }
-          attrbute_names.delete("default_attachment")
           attrbute_names.each do |column|
             row column
-          end
-          row("attachments") do |product|
-            output = ActiveSupport::SafeBuffer.new
-            attas = product.format_attachment("240x240")
-            attas = attas.map do |atta|
-              class_name = atta[:default_state] ? :default_preview : :product_preview
-              output << content_tag(:img, nil,
-                :class => class_name,
-                :src => atta[:url])
-            end
-            output
           end
         end
       end
@@ -65,13 +54,11 @@ ActiveAdmin.register Product do
   end
 
   collection_action :create_plus, :method => :post do
-    atta = dispose_options(params[:product])
-    @product = Product.new(params[:product].merge(atta))
-
+    p = params[:product]
+    @product = Product.new(p)
+    @product.attachment_ids = dispose_options(p)[:attachment_ids]
     if @product.save
       redirect_to system_product_path(@product)
-    else
-      render "new_plus"
     end
   end
 
@@ -86,13 +73,8 @@ ActiveAdmin.register Product do
   member_action :update_plus, :method => :put do
     p = params[:product]
     @product = Product.find(params[:id])
-    # xifengzhu
-    debugger
-    @product.attach_properties!
     if @product.update_attributes(p.merge(dispose_options(p)))
       redirect_to system_product_path(@product)
-    else
-      render "edit_plus"
     end
   end
 
@@ -106,26 +88,32 @@ ActiveAdmin.register Product do
     link_to 'Attach Properties', attach_properties_system_product_path(params[:id]), :method => :put
   end
 
+  def form_builder(product)
+
+  end
+
   collection_action :load_category_properties do
     root = '/panama'.to_dir
     # @product = Product.find(params[:id])
     @product = params[:product_id].blank? ? Product.new : Product.find(params[:product_id])
+
     register_value :form do
-      semantic_form_for(@product) do |f|
+      semantic_form_for @product, :method => :put do |f|
         break f
       end
     end
     @category = Category.find(params[:category_id])
     @product.category = @category
     @product.attach_properties!
-    @content = PanamaCore::Contents.fetch_for(@category, :additional_properties)
+    @content = PanamaCore::Contents.fetch_for(@category, :additional_properties_admins)
 
     if @content.nil?
-      render :text => :ok
+      render :text => '<h1>No having property!</h1>'
     else
       render_content(@content, locals: { category: @category })
     end
   end
+
 
 
   def additional_properties_content(category = nil)
