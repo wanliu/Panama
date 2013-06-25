9139c735d4bffd1de77053e901f906d457dfb6d8
xifengzhu <xifengzhu520@gmail.com>
2013-06-25 14:11:23 +0800

    tmp

--- app/controllers/product_search_controller.rb
+++ app/controllers/product_search_controller.rb
@@ -2,7 +2,7 @@ class ProductSearchController < ApplicationController
   before_filter :login_required
 
   def index
-    products = Redis::Search.complete("Product", params[:q]).first(params[:limit])
+    products = Redis::Search.complete("Product", params[:q]).first(10)
     respond_to do |format|
       format.json { render json: products.map{|p| p["value"] = p["title"];p } }
     end
