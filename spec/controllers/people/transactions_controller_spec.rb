f3a090ec6ef1e88c191ee90daffd4d78bc4a3916
kaka <huxinghai1988@gmail.com>
2013-06-26 11:34:46 +0800

    fixed

--- spec/controllers/people/transactions_controller_spec.rb
+++ spec/controllers/people/transactions_controller_spec.rb
@@ -39,20 +39,6 @@ describe People::TransactionsController, "用户订单交易流通" do
     end
   end
 
-  describe "GET new" do
-    it "显示添加页面" do
-      get :new, {:person_id => ""}, valid_session
-      assigns(:transaction).should be_a_new(OrderTransaction)
-    end
-  end
-
-  describe "GET edit" do
-    it "显示编辑页面" do
-      transaction = OrderTransaction.create! valid_attributes
-      get :edit, {:id => transaction.to_param,:person_id => ""}, valid_session
-      assigns(:transaction).should eq(transaction)
-    end
-  end
 
   describe "POST batch_create" do
     it "生成成功" do
@@ -65,93 +51,6 @@ describe People::TransactionsController, "用户订单交易流通" do
       Cart.any_instance.should_receive(:create_transaction).with(current_user).and_return(false)
       post :batch_create, person_params.merge(:id => ""), valid_session
       response.should redirect_to(person_cart_index_path(current_user.login))
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      before :each do
-        @options = person_params.merge({:order_transaction => valid_attributes})
-      end
-
-      it "附加一条记录" do
-        expect {
-          post :create, @options, valid_session
-        }.to change(OrderTransaction, :count).by(1)
-      end
-
-      it "添加 OrderTransaction对象" do
-        post :create, @options, valid_session
-        assigns(:transaction).should be_a(OrderTransaction)
-        assigns(:transaction).should be_persisted
-      end
-
-      it "添加记录重定向" do
-        post :create, @options, valid_session
-        response.should redirect_to(person_transaction_path(current_user.login, assigns(:transaction)))
-      end
-    end
-
-    describe "with invalid params" do
-      it "添加记录失败" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        OrderTransaction.any_instance.stub(:save).and_return(false)
-        post :create, person_params.merge({:transaction => {  }}), valid_session
-        assigns(:transaction).should be_a_new(OrderTransaction)
-      end
-
-      it "添加失败重新显示new" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        OrderTransaction.any_instance.stub(:save).and_return(false)
-        post :create, person_params.merge({:transaction => {  }}), valid_session
-        response.should render_template("new")
-      end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      it "修改记录" do
-        transaction = OrderTransaction.create! valid_attributes
-        OrderTransaction.any_instance.should_receive(:update_attributes).with("items_count" => "3", "total" => "6")
-        put :update, person_params.merge({
-          :id => transaction.to_param,
-          :order_transaction => { :items_count => 3, :total => 6 }}), valid_session
-      end
-
-      it "修改成功" do
-        transaction = OrderTransaction.create! valid_attributes
-        put :update, person_params.merge({
-          :id => transaction.to_param,
-          :transaction => valid_attributes}), valid_session
-        assigns(:transaction).should eq(transaction)
-      end
-
-      it "修改成功跳转" do
-        transaction = OrderTransaction.create! valid_attributes
-        put :update, person_params.merge({
-          :id => transaction.to_param,
-          :transaction => valid_attributes}), valid_session
-        response.should redirect_to(person_transaction_path(current_user.login, transaction))
-      end
-    end
-
-    describe "with invalid params" do
-      it "修改失败" do
-        transaction = OrderTransaction.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        OrderTransaction.any_instance.stub(:save).and_return(false)
-        put :update, {:id => transaction.to_param, :transaction => {  },:person_id => ""}, valid_session
-        assigns(:transaction).should eq(transaction)
-      end
-
-      it "修改失败显示编辑状态" do
-        transaction = OrderTransaction.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        OrderTransaction.any_instance.stub(:save).and_return(false)
-        put :update, {:id => transaction.to_param, :transaction => {  },:person_id => ""}, valid_session
-        response.should render_template("edit")
-      end
     end
   end
 
@@ -170,23 +69,56 @@ describe People::TransactionsController, "用户订单交易流通" do
     end
   end
 
-  describe "POST event" do
+  describe "POST event 订单状态事件" do
 
-    it "订单状态变更" do
+    it "确认订单过期" do
+      OrderTransaction.any_instance.should_receive(:valid?).and_return(true)
       transaction = OrderTransaction.create! valid_attributes
-      post :event, person_params.merge({
-        :event => :buy,
-        :id => transaction.to_param}), valid_session
-      assigns(:transaction).state.should_not eq(:order)
+      transaction
     end
 
-    it "订单状态变更成功跳转页面" do
-      transaction = OrderTransaction.create! valid_attributes
-      post :event, person_params.merge({
-        :event => :buy,
-        :id => transaction.to_param}), valid_session
-      response.should render_template(:partial => "people/transactions/_transaction")
-      #response.should redirect_to(person_transaction_path(current_user.login, transaction))
+    describe "快递运输" do
+      let(:delivery_manner){ FactoryGirl.create(:express) }
+
+      before do
+        @transaction = OrderTransaction.create! valid_attributes
+        @transaction.delivery_manner = delivery_manner
+      end
+
+      describe "在线付款" do
+        let(:online_payment){ FactoryGirl.create(:online_payment) }
+
+        before do
+          @transaction.pay_manner = online_payment
+          @transaction.save
+        end
+
+        it "确认订单 到 等待付款" do
+          @transaction.state.should eq("order")
+          post :event, person_params.merge({
+            :event => :buy,
+            :id => @transaction.to_param}), valid_session
+          assigns(:transaction).state.should_not eq("waiting_paid")
+        end
+
+      end
+
+
+      describe "货到付款" do
+      end
+
+      describe "银行汇款" do
+      end
+
     end
+
+    # it "订单状态变更成功跳转页面" do
+    #   transaction = OrderTransaction.create! valid_attributes
+    #   post :event, person_params.merge({
+    #     :event => :buy,
+    #     :id => transaction.to_param}), valid_session
+    #   response.should render_template(:partial => "people/transactions/_transaction")
+    #   #response.should redirect_to(person_transaction_path(current_user.login, transaction))
+    # end
   end
 end
--- spec/factories/delivery_manners.rb
+++ spec/factories/delivery_manners.rb
@@ -1,9 +1,15 @@
+#encoding: utf-8
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :delivery_manner do
-    code "MyString"
-    name "MyString"
-    state false
+  factory :express, :class => DeliveryManner do
+    code "express"
+    name "快递运输"
+    default_state true
+  end
+
+  factory :local_delivery , :class => DeliveryManner do
+  	code "local_delivery"
+  	name "本地送货"
   end
 end
