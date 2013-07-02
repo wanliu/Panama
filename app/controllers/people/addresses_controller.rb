7a70a365b0a4f0300f6dd035d7a5eeb757e6a83b
z2009zxiaolong <z2009zxiaolong@gmail.com>
2013-07-02 12:13:09 +0800

    modify address admin

--- app/controllers/people/addresses_controller.rb
+++ app/controllers/people/addresses_controller.rb
@@ -3,4 +3,12 @@ class People::AddressesController < People::BaseController
 	def index
 		@addresses = Address.where(:user_id => current_user.id)
 	end
+
+	def create
+		debugger
+	end
+
+	def update
+		debugger
+	end
 end
