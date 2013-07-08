5a5d038bc5a05d8b7b2235f77190fbd442e52ca5
kaka <huxinghai1988@gmail.com>
2013-07-08 18:13:06 +0800

    fixed

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,12 +4,16 @@ require 'carrierwave_upyun'
 
 CarrierWave.configure do | c |
   class ImageUploader < Kernel.const_get(c.storage.name.gsub(/::/, ""))
+
+    def default_file
+      @default_file = "file_blank.gif"
+    end
   end
 
   class AvatarUploader < ImageUploader
 
-  	def default_file 
-		@default_file = "default_avatar.jpg"
-	end
+    def default_file
+      @default_file = "default_avatar.jpg"
+    end
   end
 end
