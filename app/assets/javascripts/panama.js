05de368fd719e0630373b7bcf1098977fae59505
kaka <huxinghai1988@gmail.com>
2013-07-09 17:52:58 +0800

    ajax setup

--- app/assets/javascripts/panama.js
+++ app/assets/javascripts/panama.js
@@ -33,42 +33,53 @@ root = window || this;
 
 var panle_modal = $("#login-modal");
 AjaxAuthClient.setupRetrieveLoginUrlCallback(function(url){
-    panle_modal.find("iframe").attr("src", url);
-    panle_modal.modal("show");
+  panle_modal.find("iframe").attr("src", url);
+  panle_modal.modal("show");
 })
 
 AjaxAuthClient.registreLoginSuccess(function(user){
-    panle_modal.modal("hide");
+  panle_modal.modal("hide");
 })
 
 var load_modal_head_with_height = function(title, height){
-    panle_modal.find(".modal-header>.context").html(title);
-    panle_modal.find(".modal-body>iframe").animate({
-        height : height
-    })
+  panle_modal.find(".modal-header>.context").html(title);
+  panle_modal.find(".modal-body>iframe").animate({
+    height : height
+  })
 }
 
 AjaxAuthClient.registreLoadUserLogin(function(){
-    load_modal_head_with_height("用户登陆", "200px");
+  load_modal_head_with_height("用户登陆", "200px");
 })
 
 AjaxAuthClient.registreLoadForgotPassword(function(){
-    load_modal_head_with_height("找回密码", "130px");
+  load_modal_head_with_height("找回密码", "130px");
 })
 
 AjaxAuthClient.registreLoadCreateUser(function(){
-    load_modal_head_with_height("用户注册", "310px");
+  load_modal_head_with_height("用户注册", "310px");
 })
 
 root.loadPage = function(query, url){
-    $.get(url, {ajaxify: true}, function(data){
-        $(query).replaceWith(data);
-    });
+  $.get(url, {ajaxify: true}, function(data){
+    $(query).replaceWith(data);
+  });
 }
 
 $.ajaxSetup({
-    beforeSend: function(xhr){
-        xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
-    }
+  beforeSend: function(xhr){
+    xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))
+  }
+})
+
+$(document).ajaxError(function(xhr, error, thr){
+  if(error.status == 500){
+    $.pnotify({
+      title: '请求出错！',
+      text: error.responseText.toString(),
+      type: 'error',
+      text_escape: true
+    })
+  }
 })
 
