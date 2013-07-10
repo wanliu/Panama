f9c847e6ccb71e6d918510f11560ead17bdfbf12
rakeLL <satbirdd@gmail.com>
2013-07-10 18:30:20 +0800

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -399,7 +399,6 @@ ActiveRecord::Schema.define(:version => 20130710074116) do
     t.integer  "operator_id"
     t.string   "delivery_code"
     t.integer  "pay_manner_id"
-    t.integer  "transfer_sheet_id"
     t.integer  "delivery_manner_id"
   end
 
