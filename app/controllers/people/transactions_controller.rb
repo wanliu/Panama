9bdf6aa513122529c5f85c07fd7216fba72d106e
hysios hu <hysios@gmail.com>
2013-07-13 13:48:18 +0800

    change event params

--- app/controllers/people/transactions_controller.rb
+++ app/controllers/people/transactions_controller.rb
@@ -43,7 +43,7 @@ class People::TransactionsController < People::BaseController
   end
 
   def event
-    @transaction = current_order.find(0)
+    @transaction = current_order.find(params[:id])
     event_name = params[:event]
     authorize! :event, @transaction
 
