f4550e43d71748a6dc79b90dc7294379a771977d
z2009zxiaolong <z2009zxiaolong@gmail.com>
2013-07-18 14:53:01 +0800

    添加属性

--- app/controllers/completing_people_controller.rb
+++ app/controllers/completing_people_controller.rb
@@ -3,14 +3,20 @@ class CompletingPeopleController < Wicked::WizardController
 
   steps :pick_industry, :authenticate_license
 
-
   def show
   	@user_auth = UserAuth.new
     render_wizard
   end
 
   def update
-  	# @user_auth = UserAuth.new(params[:user_auth])
-  	# if @user_auth.valid?
+  	@user_auth = UserAuth.new(params[:user_auth])
+  	if @user_auth.valid?
+
+    else
+      format.html{ render error_back_complete_people_html }
+    end
   end
+
+  private
+
 end
