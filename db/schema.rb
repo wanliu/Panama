9bb1a81cf47b1a40ed76e82e2ecdd9c74f2f4bd4
rakeLL <satbirdd@gmail.com>
2013-07-18 11:33:10 +0800

    relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130718020711) do
+ActiveRecord::Schema.define(:version => 20130718031015) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -554,9 +554,13 @@ ActiveRecord::Schema.define(:version => 20130718020711) do
   create_table "services", :force => true do |t|
     t.string   "name",       :null => false
     t.string   "type"
-    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "services_users", :force => true do |t|
+    t.integer "user_id"
+    t.integer "service_id"
   end
 
   create_table "sessions", :force => true do |t|
