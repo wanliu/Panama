7f3f1206f0fda9dc77f6e1a2ed0d1bcd6cf33a71
rakeLL <satbirdd@gmail.com>
2013-07-18 12:00:37 +0800

    service type

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,7 @@ class Service < ActiveRecord::Base
   attr_accessible :name, :type
 
   has_and_belongs_to_many :users
+
+  validates :type, inclusion: { in: %w(buyer seller),
+    message: "%{value} is not a valid type" }
 end
