ed2e245ca6fd4805fba06920b868eefccc7d5574
z2009zxiaolong <z2009zxiaolong@gmail.com>
2013-07-23 15:02:18 +0800

    merge tire

--- app/controllers/product_search_controller.rb
+++ app/controllers/product_search_controller.rb
@@ -2,7 +2,9 @@ class ProductSearchController < ApplicationController
   before_filter :login_required
 
   def index
-    products = Redis::Search.complete("Product", params[:q]).first(10)
+    # products = Redis::Search.complete("Product", params[:q]).first(10)
+    s = Product.search2 "name:#{params[:q]}"
+    products = s.results
     respond_to do |format|
       format.json { render json: products }
     end
