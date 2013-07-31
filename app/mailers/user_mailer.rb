32c807458822f0df53f6dc783ecab194f444915e
z2009zxiaolong <z2009zxiaolong@gmail.com>
2013-07-31 17:24:20 +0800

    fixes #54

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -17,4 +17,15 @@ class UserMailer < ActionMailer::Base
   	@user_name, @rejected_reason, @url = user_name, rejected_reason, url
     mail(:to => user_mail, :subject => "[万流商城] 您的验证申请没有通过审核")
   end
+
+
+  def send_activity_checked_notify(user_mail, user_name, url)
+    @user_name, @url = user_name, url
+    mail(:to => user_mail, :subject => "[万流商城] 您的发布活动申请已经通过审核")
+  end
+
+  def send_activity_rejected_notify(user_mail, user_name, rejected_reason, url)
+    @user_name, @rejected_reason, @url = user_name, rejected_reason, url
+    mail(:to => user_mail, :subject => "[万流商城] 您的发布活动申请没有通过审核")
+  end
 end
