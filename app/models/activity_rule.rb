40f9485d6ffc608a91b0a976b6938b0b0eda07a9
xifengzhu <xifengzhu520@gmail.com>
2013-09-24 16:47:27 +0800

    fix

--- app/models/activity_rule.rb
+++ app/models/activity_rule.rb
@@ -1,5 +1,5 @@
 class ActivityRule < ActiveRecord::Base
-  attr_accessible :name, :value_type, :nvalue, :dvalue, :dtvalue, :fvalue
+  attr_accessible :name, :value_type, :nvalue, :dvalue, :dtvalue, :fvalue, :value
 
   belongs_to :activity
 end
