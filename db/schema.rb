be3a5f0e27d7d6b3cd035eb2da3dca73be8b8215
kaka <huxinghai1988@gmail.com>
2013-10-26 14:06:55 +0800

    rename table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131026053721) do
+ActiveRecord::Schema.define(:version => 20131026060427) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -160,7 +160,7 @@ ActiveRecord::Schema.define(:version => 20131026053721) do
     t.datetime "updated_at",    :null => false
   end
 
-  create_table "attachments_region", :force => true do |t|
+  create_table "attachments_regions", :force => true do |t|
     t.integer  "region_id"
     t.integer  "attachment_id"
     t.datetime "created_at",    :null => false
