bf3581d72185617d2b642419db0f6302c8c204f5
agilejzl <agilejzl@gmail.com>
2013-12-13 18:02:30 +0800

    修改通知跳转

--- app/assets/javascripts/lib/form_extend.js
+++ app/assets/javascripts/lib/form_extend.js
@@ -2,6 +2,17 @@
 //= require jquery
 
 (function($){
+  window.paramsFromUrl = function paramsFromUrl() {
+    var query = location.search.substr(1);
+    var data = query.split("&");
+    var result = {};
+    for(var i=0; i<data.length; i++) {
+      var item = data[i].split("=");
+      result[item[0]] = item[1];
+    }
+    return result;
+  }
+
   $.fn.serializeHash = function() {
     var hash = {};
     /***
