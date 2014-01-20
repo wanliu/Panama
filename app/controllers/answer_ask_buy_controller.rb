b0c39acff05ed860f69fc13769a990bcd45ef41d
xifengzhu <xifengzhu520@gmail.com>
2014-01-20 11:53:02 +0800

    fix #399

--- app/controllers/answer_ask_buy_controller.rb
+++ app/controllers/answer_ask_buy_controller.rb
@@ -10,7 +10,7 @@ class AnswerAskBuyController < ApplicationController
     respond_to do |format|
       if current_user.id == @ask_buy.user_id || !current_user.is_seller? 
         format.json{ render :json => ['不能参与自己的求购或者不是供应商！'], :status => 403 }
-      elsif @ask_buy.open == false || !current_user.answered_ask_buy(@ask_buy.id).nil?
+      elsif !@ask_buy.open || !current_user.answered_ask_buy(@ask_buy.id).nil?
         format.json{ render :json => ['您已经参与过此求购,或此求购已经被关闭'], :status => 403 }
       else
         all_params = params[:answer_ask_buy].merge!(:user_id => current_user.id)
@@ -28,21 +28,25 @@ class AnswerAskBuyController < ApplicationController
   def create_order
     @answer_ask_buy = AnswerAskBuy.find(params[:id])
     respond_to do |format|
-      @order = current_user.transactions.build(seller_id: @answer_ask_buy.seller_shop.id)
-      @item = @order.items.build({
-        :product_id  => @answer_ask_buy.shop_product.product_id,
-        :amount => @answer_ask_buy.amount,
-        :title  => @answer_ask_buy.shop_product_name,
-        :price  => @answer_ask_buy.price,
-        :user_id => @answer_ask_buy.buyer.id,
-        :shop_id => @answer_ask_buy.seller_shop.id
-      })
-      @item.buy_state = :guarantee
-      if @order.save        
-        @answer_ask_buy.convert_to_order(@order.id)
-        format.json{ render :json => @order }
+      if @answer_ask_buy.ask_buy.open
+        @order = current_user.transactions.build(seller_id: @answer_ask_buy.seller_shop.id)
+        @item = @order.items.build({
+          :product_id  => @answer_ask_buy.shop_product.product_id,
+          :amount => @answer_ask_buy.amount,
+          :title  => @answer_ask_buy.shop_product_name,
+          :price  => @answer_ask_buy.price,
+          :user_id => @answer_ask_buy.buyer.id,
+          :shop_id => @answer_ask_buy.seller_shop.id
+        })
+        @item.buy_state = :guarantee
+        if @order.save        
+          @answer_ask_buy.convert_to_order(@order.id)
+          format.json{ render :json => @order }
+        else
+          format.json{ render :json => draw_errors_message(@order), :status => 403 }
+        end
       else
-        format.json{ render :json => draw_errors_message(@order), :status => 403 }
+        format.json{ render :json => ['此求购已经被关闭,不能进行交易'], :status => 403 }
       end
     end
   end
