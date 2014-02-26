25626c78640b8f6fe6f9fc134ff42791987c8e7c
xifengzhu <xifengzhu520@gmail.com>
2014-02-26 11:49:48 +0800

    修复重新审核

--- app/controllers/admins/shops/acounts_controller.rb
+++ app/controllers/admins/shops/acounts_controller.rb
@@ -17,7 +17,6 @@ class Admins::Shops::AcountsController < Admins::Shops::SectionController
     @user_checking = current_shop.user.user_checking
     @shop = Shop.find_by(:name => params[:shop_id])
     @shop_auth = ShopAuth.new(@user_checking.attributes.merge!(:shop_name => @shop.name,:shop_summary => @shop.shop_summary))
-    @shop.shutdown_shop
   end
 
   def edit_address
