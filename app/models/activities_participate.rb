aa3945295522ce9a383c68462c2e3bb743fb85b2
xifengzhu <xifengzhu520@gmail.com>
2014-03-14 14:39:51 +0800

    修复老订单金额问题

--- app/models/activities_participate.rb
+++ app/models/activities_participate.rb
@@ -26,17 +26,30 @@ class ActivitiesParticipate < ActiveRecord::Base
     activity.update_participate
   end
 
+  #修改未付款订单的价格（当活动进行到下一阶段时）
+  def change_price_for_old_order(price)
+    transactions = ActivitiesOrderTransaction.where(:activity_id => activity.id, :state => false).includes(:order_transaction)
+    transactions.each do |t|
+      t.order_transaction.items.each do |i|
+        if i.shop_product.id == activity.shop_product.id
+          i.update_attributes(:price => price)
+        end
+      end
+    end
+  end
+
   def focus_rebate
     if activity.foucs_type?
       price = activity.focus_spread
-      activity.transactions.each do |t|
-        unless t.buyer == user
+      change_price_for_old_order(activity.focus_price)
+      activity.transactions.each do |t| 
+        unless t.items[0].price == activity.focus_price
           money = price * t.items[0].amount
           t.seller.user.payment(money, {
             :target => t.buyer,
             :owner => t,
-            :decription => "活动聚集返还金额给#{t.buyer.login}买家"})
-        end
+            :decription => "活动聚集返还金额给#{t.buyer.login}买家"}) 
+        end       
       end if price > 0
     end
   end
